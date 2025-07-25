9382c1c300d90285d79799139ac8b113af0a9fa4
chrisdvkta <chrisdevkota77@gmail.com>
2025-07-25 13:06:10 +0545

    add: roll number form

--- src/modules/Authentication/presentation/components/rollNoForm.tsx
+++ src/modules/Authentication/presentation/components/rollNoForm.tsx
@@ -1,10 +1,25 @@
+import Button from "@/core/presentation/components/Button/Button";
+import { Input } from "@/core/presentation/components/ui/input";
+import { Label } from "@/core/presentation/components/ui/label";
 import React from "react";
 
-const rollNoForm = () => {
+const RollNoForm = () => {
   return (
-    <div>rollNoForm</div>
-    // todo:roll no
+    <div className="md:w-[50%]  ">
+      <form className="flex flex-col gap-8 ">
+        <div className="flex flex-col gap-3">
+          <Label>Roll Number</Label>
+          <Input
+            type="text"
+            placeholder="Your roll number..."
+            className="w-full px-5 py-6  "
+          />
+        </div>
+
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
-export default rollNoForm;
+export default RollNoForm;
