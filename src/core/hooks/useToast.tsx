86e68ddd066714096d80cf3c9fb8293a055575f6
chrisdvkta <chrisdevkota77@gmail.com>
2025-08-11 17:56:09 +0545

    update: toast hook

--- src/core/hooks/useToast.tsx
+++ src/core/hooks/useToast.tsx
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 export const useToast = (
   type: keyof typeof toast,
   message: any,
-  duration: number | 3000,
+  duration?: number | 3000,
 ) => {
   if (typeof toast[type] === "function") {
     toast[type](message, { duration });
