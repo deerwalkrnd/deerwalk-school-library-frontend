dba4da17afaa750fd7a5eb73d50213fa4c30c97a
Supritphuyal <supritphuyal123@gmail.com>
2025-08-20 14:24:37 +0545

    Added Edit fine Model

--- src/app/api/student-profile/route.ts
+++ src/app/api/student-profile/route.ts
@@ -1,9 +1,10 @@
-import StudentProfile from "@/modules/StudentProfile/presentation/components/StudentProfile";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch(`${process.env.API_URL!}/studentProfile`);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/studentProfile`,
+    );
     if (!response.ok) {
       throw new Error(`HTTP error! status ${response.status}`);
     }
