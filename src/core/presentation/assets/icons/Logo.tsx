fd9ef680928076b6b380e68e8b162bd3da11ff0d
chrisdvkta <chrisdevkota77@gmail.com>
2025-09-03 13:59:11 +0545

    update:mobile sidebar

--- src/core/presentation/assets/icons/Logo.tsx
+++ src/core/presentation/assets/icons/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = ({ className }: { className: string }) => {
       width="160"
       height="48"
       viewBox="0 0 160 48"
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
--- src/core/presentation/components/Sidebar/Sidebar.tsx
+++ src/core/presentation/components/Sidebar/Sidebar.tsx
@@ -5,15 +5,39 @@ import { NavigationConfig } from "./NavigationList";
 import Logo from "../../assets/icons/Logo";
 import { usePathname } from "next/navigation";
 import LoginHero from "../../assets/images/LoginHero";
+import Image from "next/image";
 
-const Sidebar = () => {
+interface SidebarProps {
+  onNavigate?: () => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ onNavigate }) => {
   const { role, logout } = useAuth();
   const path = usePathname();
 
+  const handleNavigation = () => {
+    onNavigate?.();
+  };
+
+  const handleLogout = () => {
+    logout();
+    onNavigate?.();
+  };
+
   return (
     <aside className="flex flex-col p-5 h-screen min-w-72 border-r">
-      <div className="lg:mx-auto mt-10">
-        <Logo className="w-32 h-auto" />
+      <div className="block lg:hidden mx-auto mt-10">
+        <Image
+          alt="School logo"
+          src="/logo.svg"
+          width={128}
+          height={128}
+          className="w-20 sm:w-24 md:w-32 mx-auto"
+        />
+      </div>
+
+      <div className="hidden lg:block mx-auto mt-10">
+        <Logo className="w-20 sm:w-24 md:w-32 mx-auto" />
       </div>
 
       <div className="flex-1 flex flex-col gap-6 mt-10">
@@ -21,6 +45,7 @@ const Sidebar = () => {
           <a
             href={item.href}
             key={item.name}
+            onClick={handleNavigation}
             className={`flex flex-row items-center gap-5 p-3 px-4 rounded-md font-medium text-lg transition-colors ${
               path === item.href
                 ? "bg-primary text-white"
@@ -33,10 +58,9 @@ const Sidebar = () => {
         ))}
       </div>
 
-      {/* Logout */}
       <div
         className="cursor-pointer mt-auto p-3 px-4 rounded-md hover:bg-red-100 dark:hover:bg-red-900"
-        onClick={() => logout()}
+        onClick={handleLogout}
       >
         Logout
       </div>
