8460839c7984c2279ade52bace5de4a125fcf2d8
itsshahrahul <121297105+itsshahrahul@users.noreply.github.com>
2025-09-05 12:36:00 +0545

    add: user snippet for use state, add: add quote popupmodal

--- src/modules/AnnouncementModals/presentation/components/AddQuote.tsx
+++ src/modules/AnnouncementModals/presentation/components/AddQuote.tsx
@@ -6,7 +6,34 @@ import { CircleX } from "lucide-react";
 
 interface AddQuoteModalprops {
   open: boolean;
-  onOpenchange: (open: boolean) => void;
+  onOpenChange: (open: boolean) => void;
 }
 
-// export function AddQuoteModal
+export function AddQuoteModal({ open, onOpenChange }: AddQuoteModalprops) {
+  const [title, setTitle] = useState("");
+  const [animationClass, setanimationClass] = useState("");
+
+  return (
+    <div className="fixed top-0 right-0 bottom-0 left-0 md:left-64 z-50 flex items-center justify-center">
+      <div
+        className="fixed inset-0 bg-opacity-50"
+        onClick={() => onOpenChange(false)}
+        aria-hidden="true"
+      />
+
+      <div
+        className={`relative bg-white rounded-sm shadow-lg  w-127.5 mx-4 p-4 h-137 overflow-y-auto no-scrollbar ${animationClass} `}
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal title"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="p-6">
+          <div className="flex items-center justify-center mb-6">
+            <h2 id="modal-title"></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
