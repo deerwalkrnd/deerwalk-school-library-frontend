599056eb727ee317a07c03d58e297fd530ec60a5
chrisdvkta <chrisdevkota77@gmail.com>
2025-09-18 20:01:33 +0545

    update:book status

--- src/modules/Librarian/BookStatus/presentation/components/IssueBookTable.tsx
+++ src/modules/Librarian/BookStatus/presentation/components/IssueBookTable.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { DataTable } from "@/core/presentation/components/DataTable/DataTable";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { createIssueBookColumns } from "./IssueBookColumns";
+import Pagination from "@/core/presentation/components/pagination/Pagination";
 
 const IssueBookTable = () => {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+  }, []);
+
+  const currentPage = 1;
+  const totalPages = 5;
+  const hasPreviousPage = currentPage > 1;
+  const hasNextPage = true;
+
   const handleDelete = () => {};
   const handleIssue = () => {};
   const columns = useMemo(
@@ -19,6 +31,13 @@ const IssueBookTable = () => {
         enableSelection={false}
         enableFiltering={false}
         enablePagination={false}
+      />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        hasNextPage={hasNextPage}
+        hasPreviousPage={hasPreviousPage}
+        onPageChange={setPage}
       />
     </div>
   );
--- src/modules/Librarian/BookStatus/presentation/components/ReturnBookTable.tsx
+++ src/modules/Librarian/BookStatus/presentation/components/ReturnBookTable.tsx
@@ -1,11 +1,23 @@
 "use client";
 import { DataTable } from "@/core/presentation/components/DataTable/DataTable";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { createReturnBookColumns } from "./ReturnBookColumns";
+import Pagination from "@/core/presentation/components/pagination/Pagination";
 
 const ReturnBookTable = () => {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+  }, []);
+
   const handleRenew = () => {};
   const handleReturn = () => {};
+
+  const currentPage = 1;
+  const totalPages = 5;
+  const hasPreviousPage = currentPage > 1;
+  const hasNextPage = true;
 
   const columns = useMemo(
     () => createReturnBookColumns(handleRenew, handleReturn),
@@ -19,6 +31,13 @@ const ReturnBookTable = () => {
         enableSelection={false}
         enableFiltering={false}
         enablePagination={false}
+      />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        hasNextPage={hasNextPage}
+        hasPreviousPage={hasPreviousPage}
+        onPageChange={setPage}
       />
     </div>
   );
