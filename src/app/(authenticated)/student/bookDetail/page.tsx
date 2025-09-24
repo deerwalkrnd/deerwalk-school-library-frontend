f660a3a6ee820f8beabc632038b692364198a19a
Supritphuyal <supritphuyal123@gmail.com>
2025-09-24 14:06:17 +0545

    Initialized

---
+++ src/app/(authenticated)/student/bookDetail/page.tsx
@@ -0,0 +1,12 @@
+import BookDetail from "@/modules/BookDetails/presentation/components/bookDetail";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <BookDetail />
+    </div>
+  );
+};
+
+export default page;
