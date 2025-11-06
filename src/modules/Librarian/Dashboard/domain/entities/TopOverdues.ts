9ea03596df136ba3639597cd6c4404233f689a53
Supritphuyal <supritphuyal123@gmail.com>
2025-11-06 12:22:40 +0545

    Fixed bugs in librarian dashboard

--- src/modules/Librarian/Dashboard/domain/entities/TopOverdues.ts
+++ src/modules/Librarian/Dashboard/domain/entities/TopOverdues.ts
@@ -1,5 +1,14 @@
 export interface TopOverDues {
+  id?: number;
   student_name: string;
   overdue_days: number;
   borrowed_date: string | Date;
+  due_date: string | Date;
+  created_at: string;
+  book_copy?: {
+    book?: {
+      title?: string;
+      author?: string;
+    };
+  };
 }
